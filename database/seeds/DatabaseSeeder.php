c0d202980a720d1a510d564379f66cb39a1cb4a6
vagrant <vagrant@homestead>
2019-12-07 02:13:31 +0000

    Stop subcategorySeed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         Schema::disableForeignKeyConstraints();
         $this->call(CategoriesTableSeeder::class);
-        $this->call(SubcategoriesTableSeeder::class);
+        //$this->call(SubcategoriesTableSeeder::class);
         $this->call(BeaconsTableSeeder::class);
         $this->call(CategoryBeaconsTableSeeder::class);
         $this->call(ActivitiesTableSeeder::class);
