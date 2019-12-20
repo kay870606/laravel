0eb78e22fcbfe2f8bd437fc994152f83360c2be5
Kay <kay870606@gmail.com>
2019-12-20 13:52:23 +0800

    Others fixed

--- app/Http/Controllers/API/OtherController.php
+++ app/Http/Controllers/API/OtherController.php
@@ -16,6 +16,10 @@ class OtherController extends Controller
     {
         $others = [
             ['id' => 1, 'url' => url('storage/others/graph.png')],
+            ['id' => 2, 'url' => url('storage/others/0hV3RT2-7KCRx9cYMXaXsqtzAPZggHTRMQTRdYekotVylBG0lNV0JGfEs.mp4')],
+            ['id' => 3, 'url' => url('storage/others/0hG7QXYENpGBh-fZITfHc7uZztdwwEQQIUThtJfkkgTihEFVdOU0BXfk4.mp4')],
+            ['id' => 4, 'url' => url('storage/others/2ea92f78-dfdb-4d05-b197-80312b9c4d5a.jpeg')],
+            ['id' => 5, 'url' => url('storage/others/973613f0-23cf-4fdf-a1f8-749f524062f1.jpeg')],
         ];
 
         return $others;
