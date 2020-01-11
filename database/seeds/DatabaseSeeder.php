7a56a97f33151d62d183bb498779d6fcaef7ac0c
vagrant <vagrant@homestead>
2020-01-11 12:29:41 +0000

    Seed products

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,16 +13,16 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         Schema::disableForeignKeyConstraints();
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(SubcategoriesTableSeeder::class);
-        $this->call(BeaconsTableSeeder::class);
-        $this->call(CategoryBeaconsTableSeeder::class);
-        $this->call(ActivitiesTableSeeder::class);
-        $this->call(CategoryActivitiesTableSeeder::class);
+        //$this->call(CategoriesTableSeeder::class);
+        //$this->call(SubcategoriesTableSeeder::class);
+        //$this->call(BeaconsTableSeeder::class);
+        //$this->call(CategoryBeaconsTableSeeder::class);
+        //$this->call(ActivitiesTableSeeder::class);
+        //$this->call(CategoryActivitiesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(OrdersTableSeeder::class);
-        $this->call(KeywordsTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        //$this->call(OrdersTableSeeder::class);
+        //$this->call(KeywordsTableSeeder::class);
         Schema::enableForeignKeyConstraints();
     }
 }
